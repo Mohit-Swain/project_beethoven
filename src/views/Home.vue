6ab2fc7a9a785c198000aa512ca28c07d56a0c6b
Mohit-Swain <swain6564@gmail.com>
2020-12-08 18:39:51 +0530

    git init, a Vue + tailwind project

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Home">
+    {{ count }}
+    <button>Click me</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.Home {
+  @apply bg-green-300;
+}
+</style>
